8ed9d552b9219160c46aa8ed21f92bbbfcc34906
Daniel Ipince <ipince22@gmail.com>
2021-03-26 01:21:01 +0000

    dropdown

--- src/js/component/favorites.js
+++ src/js/component/favorites.js
@@ -2,19 +2,46 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import $ from "jquery";
+import Popper from "popper.js";
+
 export function Favorites() {
 	const { store, actions } = useContext(Context);
-	console.log(store.favorites);
-	let contador = store.favorites.length;
 
+	let contador = store.favorites.length;
 	return (
-		<div className="form-group">
-			<label>Select list:</label>
-			<select className="form-control" id="sel1">
-				{store.favorites.map((item, index) => {
-					return <option key={index}>{item}</option>;
-				})}
-			</select>
+		<div className="dropdown " id="dropdownDiv">
+			<button
+				className="btn btn-light dropdown-toggle"
+				data-boundary="window"
+				type="button"
+				id="dropdownMenuButton"
+				data-toggle="dropdown"
+				aria-haspopup="true"
+				aria-expanded="false">
+				Favorites {""} {contador}
+			</button>
+			<div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+				{contador == 0 ? (
+					<div id="dropdown" className="d-flex ml-4">
+						Empty
+					</div>
+				) : (
+					store.favorites.map((item, index) => {
+						return (
+							<div key={index} id="dropdown" className="d-flex flex-row">
+								<Link className="d-flex flex-grow-1" to={"/info/" + item}>
+									<span className="dropdown-item">{item}</span>
+								</Link>
+								<i className="fas fa-trash-alt mt-2 mr-2" onClick={() => actions.deleteFav(item)} />
+							</div>
+						);
+					})
+				)}
+			</div>
 		</div>
 	);
 }
